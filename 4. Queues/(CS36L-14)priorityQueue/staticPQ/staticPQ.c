890f06a312beaccd75a80d823be995563219757d
A7abhilash <a7abhilash@gmail.com>
2020-12-16 09:08:41 +0530

    added static PQ

---
+++ 4. Queues/(CS36L-14)priorityQueue/staticPQ/staticPQ.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct QUEUE{
+    int items[5];
+    int front,rear;
+};
+typedef struct QUEUE queue;
+
+queue enqueue(queue Q){
+    if(Q.rear==4){
+        printf("Queue is full\n");
+    }else{
+        printf("Enter the new item:");
+        scanf("%d",&Q.items[++Q.rear]);
+        printf("Item enqueued\n");
+    }
+    return Q;
+}
+
+queue dequeue(queue Q){
+    if(Q.front>Q.rear){
+        printf("Queue is empty\n");
+    }else{
+        int minI=0,min=Q.items[0],i;
+        for(i=Q.front;i<=Q.rear;i++){
+            if(Q.items[i]<min){
+                min=Q.items[i];
+                minI=i;
+            }
+        }
+        for(i=minI;i<Q.rear;i++){
+            Q.items[i]=Q.items[i+1];
+        }
+        Q.rear--;
+        printf("Dequeued item: %d\n",min);
+    }
+    return Q;
+}
+
+void displayQueue(queue Q){
+     if(Q.front>Q.rear){
+        printf("Queue is empty\n");
+    }else{
+        printf("Queue items:\n");
+        for(int i=Q.front;i<=Q.rear;i++){
+            printf("%d ",Q.items[i]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int choice;
+    queue Q;
+    Q.front=0;Q.rear=-1;
+    while(1){
+        printf("\n0.Exit\n1.Enqueue\n2.Dequeue\n3.Display Queue\nEnter your choice:");
+		scanf("%d", &choice);
+        
+        switch(choice){
+            case 0: exit(0); break;
+            case 1: Q=enqueue(Q); break;
+            case 2: Q=dequeue(Q); break;
+            case 3: displayQueue(Q); break;
+            default: printf("Enter proper choice\n");
+        }
+    }
+    return 0;
+}
