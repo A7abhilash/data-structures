a90516f9850499f74afc0836a265a14eae4e04dd
A7abhilash <a7abhilash@gmail.com>
2021-04-28 08:15:40 +0530

    added quick sort algo

---
+++ 3b.Quick_Sort/quick-sort.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int partition(int arr[],int start,int end){
+	int pivot=arr[end];		// End element is chosen as pivot in our program
+	int pIndex=start;
+	int temp;
+	
+	for(int i=start;i<end;i++){
+		if(arr[i]<=pivot){
+			//swap arr[i] & arr[pIndex]
+			temp=arr[i];
+			arr[i]=arr[pIndex];
+			arr[pIndex]=temp;
+			
+			pIndex++;
+		}
+	}
+	
+	//swap arr[e] & arr[pIndex]
+	temp=arr[end];
+	arr[end]=arr[pIndex];
+	arr[pIndex]=temp;
+	
+	return pIndex;
+}
+
+void quickSort(int arr[],int start,int end){
+	if(start<end){
+		int p=partition(arr,start,end);
+		quickSort(arr,start,p-1);			//Recursice QS call for left partition
+		quickSort(arr,p+1,end);			//Recursive QS call for right partition
+	}
+}
+
+int main(){
+	srand(time(0));
+	int i,n;
+	
+	printf("Enter the size \n");
+	scanf("%d",&n);
+	
+    int arr[n];
+	for(int i=0;i<n;i++){
+		arr[i]=rand() % 20000;
+	}
+
+	printf("Array elements are:\n");
+	for(int i=0;i<n;i++){
+		printf("%d ",arr[i]);
+	}printf("\n");
+	
+	quickSort(arr,0,n-1);
+	
+	printf("Sorted Array: \n");
+	for(i=0;i<n;i++)
+		printf("%d ",arr[i]);
+	// printf("\nCount: %d",count);
+	
+	return 0;
+}
