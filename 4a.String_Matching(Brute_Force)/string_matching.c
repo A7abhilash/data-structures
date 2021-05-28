6ed26dc17a938a4535954dda80c39792aa6a3b2a
A7abhilash <a7abhilash@gmail.com>
2021-05-28 16:01:38 +0530

    updated code.

--- 4a.String_Matching(Brute_Force)/string_matching.c
+++ 4a.String_Matching(Brute_Force)/string_matching.c
@@ -19,7 +19,7 @@ void stringMatch(char T[],char P[]){
         }
     }
     if(pos!=-1){
-        printf("String pattern found from position: %d to %d\n",pos,pos+m-1);   
+        printf("String pattern found from position: %d to %d\n",pos+1,pos+m);   
         printf("Count: %d\n",count);
     }else{
         printf("Pattern match not found!!!");   
@@ -29,11 +29,12 @@ void stringMatch(char T[],char P[]){
 int main(){
     char text[100],pattern[100];
     
-    printf("Enter the text:");
-    scanf("%s",text);
+    printf("Enter the text: ");
+    scanf(" %[^\n]s",text);
 
-    printf("Enter the pattern to be matched:");
-    scanf("%s",pattern);
+    printf("Enter the pattern to be matched: ");
+    scanf(" %[^\n]s",pattern);
+
 
     stringMatch(text,pattern);
 
